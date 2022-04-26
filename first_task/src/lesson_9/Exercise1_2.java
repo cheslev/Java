366295514154bda896a0322ae2ff63becd64ef22
cheslev <cheslev01@gmail.com>
2022-04-26 19:12:41 +0300

    lesson  9 ex3 fix

--- first_task/src/lesson_9/Exercise1_2.java
+++ first_task/src/lesson_9/Exercise1_2.java
@@ -5,7 +5,7 @@ public class Exercise1_2 {
     public static void main(String[] args) {
         List list = Arrays.asList(1, 2, "Hff", 1, 2);
         System.out.println(ex2(list));
-        ex3();
+
     }
 
     public static Set ex2(List arList) {
@@ -18,28 +18,5 @@ public class Exercise1_2 {
 
     }
 
-    public static void ex3() {
-        List<Integer> arList = new ArrayList<Integer>();
-        int i = 0;
-        long startTimeArList = System.nanoTime();
-        while (i < 1000000) {
-            arList.add(i);
-            i++;
-        }
-        long endTimeArList = System.nanoTime();
-        long durationArList = (endTimeArList - startTimeArList);
 
-        List<Integer> linList = new LinkedList<Integer>();
-        int j = 0;
-        long startTimeLinList = System.nanoTime();
-        while (j < 1000000) {
-            arList.add(j);
-            j++;
-        }
-        long endTimeLinList = System.nanoTime();
-        long durationLinList = (endTimeLinList - startTimeLinList);
-
-        System.out.println("Time arraylist - " + durationArList);
-        System.out.println("Time LinkedList - " + durationLinList);
-    }
 }
---
+++ first_task/src/lesson_9/Exersice_3.java
@@ -0,0 +1,72 @@
+package lesson_9;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Exersice_3
+{
+    public static void main(String[] args) {
+        long start1 = System.currentTimeMillis();
+        add1MlnToArray();
+        long end1 = System.currentTimeMillis();
+        System.out.println("Execution time for add1MlnToArray(): " + (end1-start1));
+
+        long start2 = System.currentTimeMillis();
+        add1MlnToLinkedList();
+        long end2 = System.currentTimeMillis();
+        System.out.println("Execution time for add1MlnToLinkedList(): " + (end2-start2));
+
+        long start3 = System.currentTimeMillis();
+        random100kSearchArray();
+        long end3 = System.currentTimeMillis();
+        System.out.println("Execution time for random100kSearchArray(): " + (end3-start3));
+
+        long start4 = System.currentTimeMillis();
+        random100kSearchList();
+        long end4 = System.currentTimeMillis();
+        System.out.println("Execution time for random100kSearchList(): " + (end4-start4));
+    }
+
+    public static List<Integer> add1MlnToArray(){
+        List<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < 1000001; i++) {
+            arrayList.add(i);
+        }
+        return arrayList;
+    }
+
+    public static List<Integer> add1MlnToLinkedList(){
+        List<Integer> linkedList = new LinkedList<>();
+        for (int i = 0; i < 1000001; i++) {
+            linkedList.add(i);
+        }
+        return linkedList;
+    }
+
+    public static void random100kSearchArray(){
+
+        List<Integer> integerList = add1MlnToArray();
+
+        int min = 1;
+        int max = 1000000;
+        int randomNum = (int)Math.floor(Math.random()*(max-min+1)+min);
+
+        for (int i = 0; i < 100000; i++) {
+            integerList.get(randomNum);
+        }
+    }
+
+    private static void random100kSearchList() {
+        List<Integer> linkedList = add1MlnToLinkedList();
+
+
+        int min = 1;
+        int max = 1000000;
+        int randomNum = (int)Math.floor(Math.random()*(max-min+1)+min);
+
+        for (int i = 0; i < 100001; i++) {
+            linkedList.get(randomNum);
+        }
+    }
+}
